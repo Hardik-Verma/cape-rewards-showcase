0b7743232f2f25c36d2ad568f7efba55f6e374d5
Hardik Verma <hardikverma1902@gmail.com>
2026-07-26 17:05:21 +0530

    feat: add op 404 black hole page

--- server.js
+++ server.js
@@ -15,6 +15,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', apiRoutes);
 
+// Catch-all 404 handler
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
